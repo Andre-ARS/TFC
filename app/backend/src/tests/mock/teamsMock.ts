aefb8be353d18f8757d7c8e987030da7b54d57c5
Andre <andre360.ars@gmail.com>
2022-08-19 16:09:54 -0300

    test(test): :clown_face: Creates a mock of the teams list to the tests
    Req13

---
+++ app/backend/src/tests/mock/teamsMock.ts
@@ -0,0 +1,18 @@
+import { ITeam } from "../../interfaces";
+
+const teamsList: ITeam[] = [
+  {
+    "id": 1,
+    "teamName": "Avaí/Kindermann"
+  },
+  {
+    "id": 2,
+    "teamName": "Bahia"
+  },
+  {
+    "id": 3,
+    "teamName": "Botafogo"
+  },
+]
+
+export default teamsList;
