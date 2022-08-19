a4fe8289338ae4985a08dbe191d355d962630bbc
Andre <andre360.ars@gmail.com>
2022-08-19 14:25:07 -0300

    feat(Services): :sparkles: Creates the method validateToken at the JwtService

--- app/backend/src/services/jwtService.ts
+++ app/backend/src/services/jwtService.ts
@@ -1,6 +1,8 @@
+import { StatusCodes } from 'http-status-codes';
 import * as Jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import { IUserSimple } from '../interfaces';
+import CustomError from '../helpers';
 
 dotenv.config();
 
@@ -16,5 +18,15 @@ export default class JwtService {
     const token = Jwt.sign({ data: payload }, secret, jwtConfig);
 
     return token;
+  }
+
+  static validateToken(token: string) {
+    try {
+      const userData = Jwt.verify(token, secret);
+
+      return userData;
+    } catch (error) {
+      throw new CustomError(StatusCodes.UNAUTHORIZED, 'Token must be a valid token');
+    }
   }
 }
