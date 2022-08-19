f33b0924bcf5332e91e69ec196b3d59067612e59
Andre <andre360.ars@gmail.com>
2022-08-19 16:20:51 -0300

    feat(Services): :sparkles: Creates the TeamsService
    Creates a service to connect with the Teams model, and implements the method getAll in it, to returns all the teams  of the table
    
    Req15

--- app/backend/src/services/index.ts
+++ app/backend/src/services/index.ts
@@ -1,7 +1,9 @@
 import LoginService from './loginService';
 import PasswordService from './passwordService';
+import TeamsService from './teamsService';
 
 export {
   PasswordService,
   LoginService,
+  TeamsService,
 };
---
+++ app/backend/src/services/teamsService.ts
@@ -0,0 +1,18 @@
+import { ITeam, ITeamsService } from '../interfaces';
+import Teams from '../database/models/Teams';
+
+export default class TeamsService implements ITeamsService {
+  private model;
+
+  constructor() {
+    this.model = Teams;
+  }
+
+  async getAll(): Promise<ITeam[]> {
+    const { model } = this;
+
+    const teams = await model.findAll() as ITeam[];
+
+    return teams;
+  }
+}
