83b08dbb443a28f8a37b3d02025c09b622008a46
Andre <andre360.ars@gmail.com>
2022-08-19 16:49:32 -0300

    test(test): :white_check_mark: Implements a test to the route GET/teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -16,18 +16,27 @@ describe('Teams model', () => {
   describe('Tests the /teams endpoint', async () => { 
     beforeEach(() => {
       sinon.stub(Teams, "findAll").resolves(teamsList as Teams[]);
+      sinon.stub(Teams, "findByPk").resolves(teamsList[2] as Teams);
     });
   
     afterEach(()=>{
       sinon.restore();
     })
   
-    it('Returns status 200 when succeeds', async () => {
+    it('Returns status 200, and a list of all teams', async () => {
       const { status, body } = await chai.request(app)
         .get('/teams');
   
       expect(status).to.equal(200);
       expect(body).to.be.deep.equal(teamsList);
+    });
+  
+    it('Returns status 200, and a specific team', async () => {
+      const { status, body } = await chai.request(app)
+        .get('/teams/3');
+  
+      expect(status).to.equal(200);
+      expect(body.id).to.equal(3);
     });
   })
 });
