bda6a2a0a29522446684f79bf5367083814c289a
Andre <andre360.ars@gmail.com>
2022-08-20 23:02:48 -0300

    feat(Controllers): :sparkles: Updates the method getAll at MatchesController
    Updates the getAll, to allow it to use the method filther of the service, when necessary
    
    Req20e21

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -5,11 +5,20 @@ import { IMatchesService } from '../interfaces/IService';
 export default class MatchesController {
   constructor(private service: IMatchesService) { }
 
-  async getAll(_req: Request, res: Response) {
+  async getAll(req: Request, res: Response) {
     const { service } = this;
+    if (req.query.inProgress) {
+      const { inProgress } = req.query;
+
+      const progress = inProgress === 'true';
+
+      const matches = await service.filter(progress);
+
+      return res.status(StatusCodes.OK).json(matches);
+    }
 
     const matches = await service.getAll();
 
-    res.status(StatusCodes.OK).json(matches);
+    return res.status(StatusCodes.OK).json(matches);
   }
 }
