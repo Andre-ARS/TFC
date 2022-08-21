8845700d82a8dd0af6a94a570714fb7d1e871f41
Andre <andre360.ars@gmail.com>
2022-08-20 22:55:25 -0300

    test(test): :white_check_mark: Implements tests to the /matches? endpoint

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -11,23 +11,42 @@ import matchesMock from './mock/matchesMock';
 chai.use(chaiHttp);
 
 const { expect } = chai;
-
 describe('Matches model', () => {
   describe('Tests the /matches endpoint', async () => { 
-    beforeEach(() => {
-      sinon.stub(Matches, "findAll").resolves(matchesMock as Matches[]);
-    });
-  
     afterEach(()=>{
       sinon.restore();
     })
-  
+    
     it('Returns status 200, and a list of all matches', async () => {
+      sinon.stub(Matches, "findAll").resolves(matchesMock as Matches[]);
+
       const { status, body } = await chai.request(app)
-        .get('/matches');
-  
+      .get('/matches');
+      
       expect(status).to.equal(200);
       expect(body).to.be.deep.equal(matchesMock);
+    });
+
+    it('When filtered returns only the matches in progress', async () => {
+      const progressMatches = matchesMock.filter(({ inProgress }) => inProgress) 
+      sinon.stub(Matches, "findAll").resolves(progressMatches as Matches[]);
+      
+      const { status, body } = await chai.request(app)
+        .get('/matches?inProgress=true');
+  
+      expect(status).to.equal(200);
+      expect(body).to.be.deep.equal(progressMatches);
+    });
+    
+    it('When filtered returns only the finished matches', async () => {
+      const finishedMatches = matchesMock.filter(({ inProgress }) => !inProgress) 
+      sinon.stub(Matches, "findAll").resolves(finishedMatches as Matches[]);
+      
+      const { status, body } = await chai.request(app)
+        .get('/matches?inProgress=false');
+  
+      expect(status).to.equal(200);
+      expect(body).to.be.deep.equal(finishedMatches);
     });
   })
 });
