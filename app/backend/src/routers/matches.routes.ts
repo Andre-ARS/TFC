504e60d5d18d6ca025d95a0f60f3824cc9a53422
Andre <andre360.ars@gmail.com>
2022-08-21 21:37:00 -0300

    feat(routes): :sparkles: Creates the endpoint PATCH/matches/id/finish to change the state of a matche
    Req24

--- app/backend/src/routers/matches.routes.ts
+++ app/backend/src/routers/matches.routes.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { tokenMiddleware } from '../middlewares';
 import MatchesController from '../controllers/matchesController';
 import MatchesService from '../services/matchesService';
 
@@ -8,6 +9,11 @@ const matchesService = new MatchesService();
 const matchesController = new MatchesController(matchesService);
 
 matchesRouter.get('/', (req, res) => matchesController.getAll(req, res));
-matchesRouter.post('/', (req, res) => matchesController.create(req, res));
+matchesRouter.post('/', tokenMiddleware, (req, res) => matchesController.create(req, res));
+matchesRouter.patch(
+  '/:id/finish',
+  tokenMiddleware,
+  (req, res) => matchesController.finish(req, res),
+);
 
 export default matchesRouter;
