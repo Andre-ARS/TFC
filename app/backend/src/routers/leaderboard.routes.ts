99d7cb235628cf4b9a6b9b0e5412a07ecf46f5c8
Andre <andre360.ars@gmail.com>
2022-08-23 17:48:13 -0300

    feat(routes): :sparkles: Creates the route GET/leaderboard/away to create an leaderboard of the away teams
    Req31e32

--- app/backend/src/routers/leaderboard.routes.ts
+++ app/backend/src/routers/leaderboard.routes.ts
@@ -8,5 +8,6 @@ const leaderboardService = new LeaderboardService();
 const leaderboardController = new LeaderboardController(leaderboardService);
 
 leaderboardRouter.get('/home', (req, res) => leaderboardController.getHomeBoard(req, res));
+leaderboardRouter.get('/away', (req, res) => leaderboardController.getAwayBoard(req, res));
 
 export default leaderboardRouter;
